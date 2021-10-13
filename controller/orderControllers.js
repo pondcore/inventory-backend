7cc60e7d8c6baa19d7653b27ed699dbbaf1025f5
a-tichat <atichat.eng@gmail.com>
2021-10-13 12:30:34 +0700

    search function

--- controller/orderControllers.js
+++ controller/orderControllers.js
@@ -1,5 +1,6 @@
 const Order = require('../models/Order');
-const mongoose = require("mongoose");
+const Customer = require('../models/Customer');
+const Product = require('../models/Product');
 
 const index = async (req, res) => {
 	try {
@@ -7,17 +8,53 @@ const index = async (req, res) => {
 		const fields = req.query.field || {};
 		const limit = parseInt(req.query.pageSize || 10);
 		const page = parseInt(req.query.current || 1);
-		let query = null;
+		const searchField = req.query.qField;
+		let filterOrder = null;
+		if (search != null && searchField == 'customer') {
+			let filterQuery = null;
+			if (search.indexOf(' ') >= 0) {
+				let [fname, lname] = search.split(' ')
+				filterQuery = {
+					$and: [
+						{ "firstname": { $regex: `.*${fname}.*` } },
+						{ "lastname": { $regex: `.*${lname}.*` } },
+					]
+				};
+			} else {
+				filterQuery = {
+					$or: [
+						{ "firstname": { $regex: `.*${search}.*` } },
+						{ "lastname": { $regex: `.*${search}.*` } }
+					],
+				};
+			}
+			const customers = await Customer.find(filterQuery).select('_id');
+			filterOrder = { 'customer_id': { $in: customers.map(item => item._id) } }
+		} else if (search != null && searchField == 'product_name') {
+			let filterQuery = null;
+			filterQuery = { "product_name": { $regex: `.*${search}.*` } };
+			const products = await Product.find(filterQuery).select('_id');
+			filterOrder = { 'products.id': { $in: products.map(item => item._id) } }
+		} else if (search != null && searchField == 'product_sku') {
+			let filterQuery = null;
+			filterQuery = { "sku": { $regex: `.*${search}.*` } };
+			const products = await Product.find(filterQuery).select('_id');
+			filterOrder = { 'products.id': { $in: products.map(item => item._id) } }
+		}
 
-		const data = await Order.find(query)
-			.populate('customer_id', 'prefix firstname lastname phone -_id')
-			.populate('products.id')
+		const data = await Order.find(filterOrder)
+			.populate({
+				path: 'customer_id',
+				select: 'prefix firstname lastname phone -_id',
+			})
+			.populate('products.id', '-image')
 			.select(fields)
 			.limit(limit)
 			.skip(limit * (page - 1))
 			.sort({ 'createdAt': -1 });
 
-		const totalOrder = await Order.countDocuments(query);
+
+		const totalOrder = await Order.countDocuments(filterOrder);
 
 		res.status(200).json({
 			success: true,
